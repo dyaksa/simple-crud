b77b2b87eb82dff94c3c4a5de33da2369117f8af
dyaksa <diasnour0395@gmail.com>
2020-08-22 12:43:08 +0700

    finish crud

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -28,6 +29,51 @@ app.get("/", (req, res) => {
 
 app.get("/add", (req, res) => {
   res.render("tambah.ejs");
+});
+
+app.post("/add", (req, res) => {
+  let name = req.body.nama;
+  let keterangan = req.body.keterangan;
+  let harga = req.body.harga;
+  let jumlah = req.body.jumlah;
+  connection.query(
+    "INSERT INTO produk(nama_produk,keterangan,harga,jumlah) VALUES(?, ?, ?, ?)",
+    [name, keterangan, harga, jumlah],
+    (err, results) => {
+      if (!err) {
+        res.redirect("/");
+      }
+    }
+  );
+});
+
+app.get("/edit/:id", (req, res) => {
+  const ID = req.params.id;
+  connection.query(
+    "SELECT * FROM produk WHERE id = ?",
+    [ID],
+    (err, results) => {
+      res.render("edit.ejs", { item: results[0] });
+    }
+  );
+});
+
+app.post("/update/:id", (req, res) => {
+  let ID = req.params.id;
+  let nama = req.body.nama;
+  let keterangan = req.body.keterangan;
+  let harga = req.body.harga;
+  let jumlah = req.body.jumlah;
+
+  connection.query(
+    "UPDATE produk SET nama_produk = ?, keterangan = ?, harga = ?, jumlah = ? WHERE id = ?",
+    [nama, keterangan, harga, jumlah, ID],
+    (err, results) => {
+      if (!err) {
+        res.redirect("/");
+      }
+    }
+  );
 });
 
 app.get("/delete/:id", (req, res) => {
